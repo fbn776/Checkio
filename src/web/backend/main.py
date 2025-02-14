0e1cd9724b9754b24d15964523828c91e3cf8959
fbn776 <fbn776@gmail.com>
2025-02-14 09:51:57 +0530

    feat: serve frontend using flask

--- src/web/backend/main.py
+++ src/web/backend/main.py
@@ -1,8 +1,7 @@
 import logging
 import os
-
 import click
-from flask import Flask, render_template, request
+from flask import Flask, render_template, send_from_directory, abort, request
 from flask_cors import CORS
 
 from utils.get_local_ip import get_local_ip
@@ -13,6 +12,19 @@ CORS(app)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+ALLOWED_EXTENSIONS = {".js", ".css", ".svg", ".png", ".jpg"}
+
+def is_safe_file(filename):
+    _, ext = os.path.splitext(filename)
+    return ext in ALLOWED_EXTENSIONS
+
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    if not is_safe_file(filename):
+        abort(403)  # Forbidden if file type is not allowed
+    return send_from_directory(os.path.join(app.root_path, 'templates/assets'), filename)
+
 
 @app.get('/is-alive')
 def is_alive():
@@ -62,7 +74,7 @@ def run_server(port = 5000):
     log.setLevel(logging.ERROR)
     click.echo = lambda *args, **kwargs: None
 
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False, )
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
 
 # Example CLI command
