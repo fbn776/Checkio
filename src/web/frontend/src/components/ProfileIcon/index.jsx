4fd7cc911ae1499025ce3a253384d86f84d463ca
abhiram <abhiramashok1062004@gmail.com>
2025-02-15 20:58:20 +0530

    Home Layout

---
+++ src/web/frontend/src/components/ProfileIcon/index.jsx
@@ -0,0 +1,40 @@
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function ProfileIcon() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div className="h-[45px] w-[45px] rounded-full bg-white hover:cursor-pointer"></div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-45 mr-5 mt-2">
+                <DropdownMenuLabel className="text-[#171717]">Profile</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>
+                        Account Details
+                    </DropdownMenuItem>
+                    <DropdownMenuItem  className="">
+                        Login
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Logout
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Sign In
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        Sign Out
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
