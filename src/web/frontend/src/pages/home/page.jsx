9e432c5efc7e410930c3c2ecbf07edc92e2ad921
abhiram <abhiramashok1062004@gmail.com>
2025-02-15 17:35:45 +0530

    Home Layout

--- src/web/frontend/src/pages/home/page.jsx
+++ src/web/frontend/src/pages/home/page.jsx
@@ -4,8 +4,15 @@ export default function HomePage() {
             <div className="w-[300px] bg-[#081627] min-h-[100vh]">
             </div>
             <div className="flex flex-grow flex-col items-center justify-start min-h-[100vh]">
-                <div className="bg-[#009be5] min-h-[150px] w-full">
-
+                <div className="flex flex-col bg-[#009be5] min-h-[150px] w-full items-center justify-between">
+                    <div className="w-full p-6">
+                        <div className="h-[60px] w-[60px] rounded-full bg-white"></div>
+                    </div>
+                    <div className="w-full flex items-center justify-start px-8 text-white">
+                        <div className="min-h-[30px] w-full">
+                            HOME
+                        </div>
+                    </div>
                 </div>
                 <div className="bg-white w-full flex-grow">
 
