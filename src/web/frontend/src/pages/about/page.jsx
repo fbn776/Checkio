190f063b5ca5120f5c8b95ec66aa4dabee67b082
Aysha022 <ayshanaurin22@gmail.com>
2025-03-05 22:47:05 +0530

    initail commit

--- src/web/frontend/src/pages/about/page.jsx
+++ src/web/frontend/src/pages/about/page.jsx
@@ -3,6 +3,8 @@
 export default function AboutPage() {
     return <div>
         <h1 className="bg-[#ee88ee] border-2 border-red-600 text-blue-500 px-5 py-[20px] max-md:bg-red-500">About</h1>
-        <p>I</p>
+        <p>
+            njgfuuhvujyujvhfu
+        </p>
     </div>
 }
