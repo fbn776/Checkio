525609c90ec5c6427027c17c1a1d6d50c4cb03c4
fbn776 <fbn776@gmail.com>
2025-03-07 03:52:32 +0530

    fix: validating user now checks if they are admin

--- src/core/auth/validate_user.py
+++ src/core/auth/validate_user.py
@@ -6,16 +6,21 @@ from utils.hashing import check_password_hash
 
 console = Console()
 
-def is_valid_user(in_username = None, in_password = None) -> bool:
+
+def is_valid_user(in_username=None, in_password=None, admin_only=True) -> bool:
     """Check if the user is valid
     :param in_username: The username to check (optional ; If not provided, it will prompt the user)
     :param in_password: The password to check (optional ; If not provided, it will prompt the user)
+    :param admin_only: If True, only check for admin users
     """
     username = in_username or click.prompt("Username", type=str)
     password = in_password or click.prompt("Password", type=str, hide_input=True)
 
     db = next(get_db())
-    user = db.query(User).filter_by(username=username).first()
+    user = db.query(User).filter_by(username=username, role="admin").first() if admin_only else (
+        db.query(User).filter_by(username=username).first()
+    )
+
     try:
         if user and check_password_hash(password, user.password):
             return True
@@ -23,4 +28,3 @@ def is_valid_user(in_username = None, in_password = None) -> bool:
             return False
     except Exception as e:
         return False
-
