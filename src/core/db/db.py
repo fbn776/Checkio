9af26f6c36f9e575ce50a80e22fafcbc396d5c3d
fbn776 <fbn776@gmail.com>
2025-03-07 03:49:03 +0530

    fix: integrity error fix

--- src/core/db/db.py
+++ src/core/db/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event, Engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///checkio.db"
@@ -6,6 +6,12 @@ DATABASE_URL = "sqlite:///checkio.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
 
 def get_db():
     db = SessionLocal()
