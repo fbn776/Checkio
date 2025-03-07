e8661d27309040c77d5645b16137f24d01e95288
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2025-03-07 14:27:43 +0530

    home page, circular graph

--- src/web/frontend/src/pages/home/page.jsx
+++ src/web/frontend/src/pages/home/page.jsx
@@ -1,7 +1,63 @@
+import CircularProgress from "./CircularProgress";
 
 
 export default function HomePage() {
-    return <div className={'p-2'}>
-        <h1>Home Page</h1>
+    return <div>
+        {/* home */}
+        <div className={'bg-[#009BE5] '}>
+            <h1 className='p-[10px] ml-[5px] w-[120px] border-b-3 text-center font-medium text-white h-[40px]'>HOME</h1>
+        </div>
+        {/* line */}
+        <div>
+            <h1 className="m-[40px] mb-[5px] font-semibold text-black text-[50px]">Streamline Evaluation – </h1>
+            <h1 className="mt-0 ml-[40px] mb-[10px] font-medium text-black text-[45px] p-0">Define, Test, and Assess with Checkio!</h1>
+        </div>
+
+        <div className="h-screen flex flex-col p-[40px] gap-[40px]">
+            {/* horizontal divs */}
+            <div className="w-full flex flex-row gap-[40px]">
+                {/* Recently Generated report */}
+                <div className="w-full bg-[#fff] shadow-[0px_0px_20px_2px_#c4c4c4] h-auto p-[20px] flex flex-col gap-[20px] hover:bg-[#e8f6fc]">
+                    <h1 className="text-[22px] font-medium text-[#009BE5] font-semibold">View Report</h1>
+                    <div className="flex gap-[10px] ml-[20px]">
+                        <div className="flex flex-col gap-[2px] w-full">
+                            <p className="text-[#9c9c9c]">Submissions: <span className="font-light">75</span></p>
+                            <p className="text-[#9c9c9c]">Most Passed Test: <span className="font-light">12</span></p>
+                            <p className="text-[#9c9c9c]">Most Failed Test: <span className="font-light">8</span></p>
+                        </div>
+                        <div className="w-[200px] h-full flex justify-center items-top">
+                            <CircularProgress value={55} label="Pass Percent" />    {/* value = pass percent of the test case*/}
+                        </div>
+                    </div>
+                </div>
+                {/* Lab preview */}
+                <div className="w-full bg-[#fff] shadow-[0px_0px_20px_2px_#c4c4c4] h-auto p-[20px] flex flex-col gap-[20px] hover:bg-[#e8f6fc]">
+                    <h1 className="text-[22px] font-medium text-[#009BE5] font-semibold">XXX Lab</h1>
+                    <div className="flex gap-[10px] ml-[20px]">
+                        <div className="flex flex-col gap-[2px] w-full">
+                            <p className="text-[#9c9c9c]">Created Test Cases: <span className="font-light">15</span></p>
+                            <p className="text-[#9c9c9c]">Submissions: <span className="font-light">12</span></p>
+                            <p className="text-[#9c9c9c]">Report Generated: <span className="font-light">10</span></p>
+                        </div>
+                        <div className="w-[200px] h-full flex justify-center items-center">
+                            <CircularProgress value={55} label="Lab Completed" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* recents */}
+            <div className="w-full flex flex-col gap-[10px]">
+                <h1 className="text-[22px] font-medium text-[#009BE5] font-semibold">Recents</h1>
+                {/* recent test cases */}
+                <div className="w-full h-auto flex flex-col gap-[2px] items-start shadow-[0px_0px_20px_2px_#c4c4c4] bg-[#c4c4c4]">
+                    <button className="w-full h-[40px] px-[40px] flex justify-between items-center bg-[#fff] hover:bg-[#e8f6fc]">#test case 1 <span className="w-[20px]">8</span></button>
+                    <button className="w-full h-[40px] px-[40px] flex justify-between items-center bg-[#fff] hover:bg-[#e8f6fc]">#test case 1 <span className="w-[20px]">8</span></button>
+                    <button className="w-full h-[40px] px-[40px] flex justify-between items-center bg-[#fff] hover:bg-[#e8f6fc]">#test case 1 <span className="w-[20px]">8</span></button>
+                    <button className="w-full h-[40px] px-[40px] flex justify-between items-center bg-[#fff] hover:bg-[#e8f6fc]">#test case 1 <span className="w-[20px]">8</span></button>
+                    <button className="w-full h-[40px] px-[40px] flex justify-between items-center bg-[#fff] hover:bg-[#e8f6fc]">#test case 1 <span className="w-[20px]">8</span></button>
+                </div>
+            </div>
+        </div>
+
     </div>
 }
