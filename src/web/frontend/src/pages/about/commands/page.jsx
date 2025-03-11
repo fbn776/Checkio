bddaf5948594f294d7fa5b7e0ca50987a3769fca
Aysha022 <ayshanaurin22@gmail.com>
2025-03-11 11:29:05 +0530

    Updates commands in About page

--- src/web/frontend/src/pages/about/commands/page.jsx
+++ src/web/frontend/src/pages/about/commands/page.jsx
@@ -6,16 +6,16 @@
                 <h2 className="text-[#081627] text-[30px] font-semibold mb-4">
                     Checkio Commands
                 </h2>
-                <ul className="list-disc pl-8 text-[#081627] text-[18px]">
-                    <li><b>checkio config : </b>used to configure the necessary settings</li>
-                    <li><b>checkio create : </b>used to create new testcases</li>
-                    <li><b>checkio about : </b>used to display the features and commands of checkio</li>
-                    <li><b>checkio serve : </b>used to access the web</li>
-                    <li><b>checkio user create : </b>used to create a new Faculty account</li>
-                    <li><b>checkio user delete : </b>used to delete an existing Faculty account</li>
-                    <li><b>checkio run : </b>used to run the program submitted by students</li>
-                    <li><b>checkio analyze : </b>used to analyze the program submitted by students</li>
-                    <li><b>checkio submit : </b>used to submit the program by students</li>
+                <ul className="list-disc pl-7 text-[#081627] text-[18px]">
+                    <li className="mb-2"><b>checkio config : </b>used to configure the necessary settings</li>
+                    <li className="mb-2"><b>checkio create : </b>used to create new testcases</li>
+                    <li className="mb-2"><b>checkio about : </b>used to display the features and commands of checkio</li>
+                    <li className="mb-2"><b>checkio serve : </b>used to access the web</li>
+                    <li className="mb-2"><b>checkio user create : </b>used to create a new Faculty account</li>
+                    <li className="mb-2"><b>checkio user delete : </b>used to delete an existing Faculty account</li>
+                    <li className="mb-2"><b>checkio run : </b>used to run the program submitted by students</li>
+                    <li className="mb-2"><b>checkio analyze : </b>used to analyze the program submitted by students</li>
+                    <li className="mb-2"><b>checkio submit : </b>used to submit the program by students</li>
                 </ul>
             </div>
       </div>
