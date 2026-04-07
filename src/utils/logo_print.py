c1bbaf4232e6e515448be3eed977ceeb2d28367c
fbn776 <fbn776@gmail.com>
2026-04-07 12:11:46 +0530

    fix: add credits

--- src/utils/logo_print.py
+++ src/utils/logo_print.py
@@ -17,5 +17,11 @@ def show_logo():
      *****************     
         ***********        
             ***""", fg="bright_blue")} 
+            
+
+Built by Aysha Naurin, Abhiram Ashok, Febin Nelson P, Sreelakshmi K of Batch 2022-26 CSE as part of our mini project.
+Under the guidance of Dr. Shibu Kumar KB.
+
+---------------------------------------------------------
     """
     return logo
